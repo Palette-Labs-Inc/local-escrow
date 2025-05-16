0ac9040a809cac05f04f3da1ec5bff799b9fdbb8
Mperhats <mperhats2@gmail.com>
2025-05-16 14:51:16 -0600

    wip

--- apps/react/src/hooks.ts
+++ apps/react/src/hooks.ts
@@ -1,13 +1,16 @@
 import { exp1Address, exp1Config } from './contracts/contracts.ts'
 import { useAccount, useReadContract } from 'wagmi'
-import { type Address, type Hex, Value } from 'ox'
+import { Value } from 'ox'
 
 export function useBalance() {
   const { address } = useAccount()
 
+  if (!address) {
+    return '0 EXP'
+  }
+
   const { data: balance } = useReadContract({
-    // biome-ignore lint/style/noNonNullAssertion: <explanation>
-    args: [address!],
+    args: [address],
     abi: exp1Config.abi,
     functionName: 'balanceOf',
     address: exp1Address,
@@ -15,27 +18,4 @@ export function useBalance() {
   })
 
   return `${Value.formatEther(balance ?? 0n)} EXP`
-}
-
-export interface DebugData {
-  transactions: Array<{
-    id: number
-    role: 'session' | 'admin'
-    created_at: string
-    address: Address.Address
-    hash: Address.Address
-    public_key: Hex.Hex
-  }>
-  key: {
-    account: Address.Address
-    privateKey: Address.Address
-  }
-  schedules: Array<{
-    id: number
-    created_at: string
-    address: Address.Address
-    schedule: string
-    action: string
-    calls: string
-  }>
 }
