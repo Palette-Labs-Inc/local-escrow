8336794b582499a821512a60ca507c0bb824809e
Mperhats <mperhats2@gmail.com>
2025-05-16 16:48:01 -0600

    wip

--- apps/react/src/hooks.ts
+++ apps/react/src/hooks.ts
@@ -4,17 +4,15 @@ import { Value } from 'ox'
 
 export function useBalance() {
   const { address } = useAccount()
-
-  if (!address) {
-    return '0 EXP'
-  }
-
   const { data: balance } = useReadContract({
-    args: [address],
     abi: exp1Config.abi,
     functionName: 'balanceOf',
     address: exp1Address,
-    query: { enabled: !!address, refetchInterval: 2_000 },
+    args: address ? [address] : undefined,
+    query: {
+      enabled: !!address,
+      refetchInterval: 2_000,
+    },
   })
 
   return `${Value.formatEther(balance ?? 0n)} EXP`
