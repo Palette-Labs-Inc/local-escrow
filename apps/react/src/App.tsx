b0012dc424d1347ab99e10671592110fa2e7ea0d
Mperhats <mperhats2@gmail.com>
2025-05-17 11:45:12 -0600

    wip layout

--- apps/react/src/App.tsx
+++ apps/react/src/App.tsx
@@ -1,9 +1,18 @@
+import { RouterProvider } from '@tanstack/react-router'
+import { WagmiProvider } from 'wagmi'
+import { queryClient, wagmiConfig } from './config.ts'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import * as Router from './lib/Router'
 
 export function App() {
   return (
-    <div className="max-w-2xl mx-auto p-6 space-y-8">
-      <p>hello world</p>
-    </div>
+	<WagmiProvider config={wagmiConfig}>
+	<QueryClientProvider client={queryClient}>
+	  <RouterProvider router={Router.router} />
+	  <ReactQueryDevtools initialIsOpen={false} />
+	</QueryClientProvider>
+  </WagmiProvider>
   )
 }
 
--- apps/react/src/main.tsx
+++ apps/react/src/main.tsx
@@ -1,33 +1,13 @@
 import './main.css'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
 import { StrictMode } from 'react'
-import { WagmiProvider } from 'wagmi'
 import { createRoot } from 'react-dom/client'
-import { queryClient, wagmiConfig } from './config.ts'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
-// Create the router instance from the generated route tree
-const router = createRouter({ routeTree })
-
-// Augment TanStack Router's register interface for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
+import { App } from './App.tsx'
 
 const root = document.querySelector('div#root')
 if (!root) throw new Error('Root not found')
 
 createRoot(root).render(
   <StrictMode>
-      <WagmiProvider config={wagmiConfig}>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </WagmiProvider>
+      <App />
   </StrictMode>
 )
