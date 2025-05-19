93c78186b7b002c7febe1098be56c25d25428460
Ivan P <ivan@noshdelivery.co>
2025-05-19 14:45:25 -0600

    Remove old client

--- apps/merchant/src/components/catalog/Collection.tsx
+++ apps/merchant/src/components/catalog/Collection.tsx
@@ -1,25 +1,41 @@
 import { useCatalog } from '#/hooks/use-catalog'
 import { useState } from 'react'
 import Item from './Item'
-
+import { EditOrCreateItem } from './Item'
+import { AtUri } from '@atproto/api'
 const LINK_CLASSES = 'inline-block cursor-pointer ml-2 font-semibold text-blue-600'
 
 export default function Collection({ collectionId }: { collectionId: string }) {
   const { catalogs, putRecord } = useCatalog()
-  const collection = catalogs?.collections[collectionId]
-  const [editingItems, setEditingItems] = useState<Set<string>>(new Set())
+  const collection = catalogs?.collections[collectionId]!
+  const [creatingItem, setCreatingItem] = useState(false)
 
-  const handleNewItemPress = () => {}
-
-  const itemMap = catalogs?.items
-
-  const newItem = (
-    <div>
-      <input type="text" placeholder="Item Name" />
-      <input type="number" placeholder="Item Price" />
-      <button onClick={handleNewItemPress}>Add</button>
-    </div>
-  )
+  const handleCreateItem = (name: string, price: number) => {
+    putRecord(
+      {
+        type: 'xyz.noshdelivery.v0.catalog.item',
+        object: {
+          name,
+          priceMoney: { amount: price, currency: 'USD' },
+        },
+      },
+      {
+        onSuccess: (uri) => {
+          // Now update the collection to include the new item
+          const itemUri = new AtUri(uri)
+          const itemId = itemUri.rkey
+          putRecord({
+            type: 'xyz.noshdelivery.v0.catalog.collection',
+            object: {
+              ...collection,
+              items: [...collection.items, itemId],
+            },
+          })
+          setCreatingItem(false)
+        },
+      },
+    )
+  }
 
   const handleRemoveItem = (itemId: string) => {
     putRecord({
@@ -34,9 +50,13 @@ export default function Collection({ collectionId }: { collectionId: string }) {
   return (
     <div>
       <div className="mt-8 mb-4 font-bold">
-        {collection?.name} <span className={LINK_CLASSES}>+</span>
+        {collection?.name}{' '}
+        <span className={LINK_CLASSES} onClick={() => setCreatingItem(true)}>
+          +
+        </span>
       </div>
       <div>
+        {creatingItem && <EditOrCreateItem onConfirm={handleCreateItem} onCancel={() => setCreatingItem(false)} />}
         {collection?.items?.length ? (
           collection?.items?.map((itemId) => (
             <Item key={itemId} itemId={itemId} onRemove={() => handleRemoveItem(itemId)} />
